d421c9cf53080c33e27854c3f1961bcef300c64a
Vibhuti Gupta <QAIT\vibhutigupta@asda1716po02925.qait.com>
2017-07-07 16:25:51 +0530

    cucumberproject

---
+++ src/main/java/cucumberTest/SeleniumTest.java
@@ -0,0 +1,56 @@
+package cucumberTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumTest {
+
+	
+		// TODO Auto-generated method stub
+	private static WebDriver driver = null;
+	
+	 
+		
+	public static void main(String[] args) {
+		// Create a new instance of the Firefox driver
+		System.setProperty("webdriver.firefox.marionette","/home/vibhutigupta/Downloads/geckodriver.exe");
+		
+        driver = new FirefoxDriver();
+ 
+        //Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception
+ 
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+ 
+        //Launch the Online Store Website
+ 
+        driver.get("http://webmail.qainfotech.com");
+ 
+        // Find the element that's ID attribute is 'account'(My Account) 
+ 
+        driver.findElement(By.xpath(".//*[@id='username']")).sendKeys("vibhutigupta");;
+        
+ 
+        // Find the element that's ID attribute is 'log' (Username)
+ 
+        // Enter Username on the element found by above desc.
+ 
+        driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("Qait@123"); 
+ 
+        // Find the element that's ID attribute is 'pwd' (Password)
+ 
+        // Enter Password on the element found by the above desc.
+ 
+        driver.findElement(By.xpath("html/body/div/div[1]/div[1]/form/table/tbody/tr[3]/td[2]/input[2]")).click();
+ 
+        
+        // Close the driver
+ 
+        driver.quit();
+ 
+
+	}
+
+}
